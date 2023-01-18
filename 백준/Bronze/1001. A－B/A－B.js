4f027a69e2a6e204c88ebd7eeffcd50abf421a22
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-01-18 17:52:47 +0900

    [Bronze V] Title: A-B, Time: 152 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+input = input[0];
+input = input.split(' ').map((item) => +item);
+solution(input[0], input[1]);
+function solution(A, B) {
+    
+    console.log(A - B);
+}
