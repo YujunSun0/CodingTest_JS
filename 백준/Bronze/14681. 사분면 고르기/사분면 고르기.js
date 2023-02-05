92ce9a0011ba65f1dfed1f1d39076d5f645e70c6
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-02-05 10:34:27 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.js
@@ -1,11 +1,6 @@
-const [x, y] = require("fs").readFileSync(0).toString().trim().split('\n').map(Number);
+const fs = require('fs');
 
-if(x >0 && y > 0){
-    console.log(1)
-} else if(x < 0 && y > 0) {
-    console.log(2)
-} else if(x > 0 && y < 0){
-    console.log(4)
-}  else{
-    console.log(3)
-}
+const [x, y] = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+if(x > 0) y > 0 ? console.log(1) : console.log(4)
+if(x < 0) y > 0 ? console.log(2) : console.log(3)
