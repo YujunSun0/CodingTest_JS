94ecc1bf5debe1ff4fbad4d01a215c5411516244
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-02-19 22:33:31 +0900

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 9360 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -1,8 +1,10 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
 const N = +input[0];
+let sum = "";
 
 for(let i=1;i<=N;i++){
     let result = input[i].split(" ").map(el=>+el);
-    console.log(`Case #${i}: ${result[0]+result[1]}`)
+    sum += `Case #${i}: ${result[0] + result[1]}\n`;
 }
+console.log(sum);
