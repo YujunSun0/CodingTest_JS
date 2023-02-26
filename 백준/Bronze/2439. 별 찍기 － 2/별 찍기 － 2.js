d1b46a40ee279398c3422509d96a2b2572d08070
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-02-26 22:09:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 9528 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -1,13 +1,5 @@
 const input = require("fs").readFileSync("/dev/stdin").toString();
 
-let result = '';
-let blank = '';
-
 for(let i=1; i<= +input; i++){
-    result += "*";
-     for(let j=0; j< +input-i; j++){
-         blank += " ";
-     }
-    console.log(blank + result);
-    blank = "";
+    console.log(" ".repeat(+input-i) + "*".repeat(i));
 }
