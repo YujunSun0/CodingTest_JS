c95baf89c092971647e717bea01804b4e9a0463f
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-03-19 14:04:21 +0900

    [Bronze V] Title: 문자와 문자열, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -1,6 +1,3 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-const arr = input[0].split("");
-const i = +input[1];
-
-console.log(arr[i-1]);
+console.log(input[0][input[1]-1])
