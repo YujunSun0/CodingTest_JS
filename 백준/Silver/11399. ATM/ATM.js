569997cb206ad9fa8a494de70e3ab924d3d3f842
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-08-14 22:01:29 +0900

    [Silver IV] Title: ATM, Time: 204 ms, Memory: 11800 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.js
@@ -0,0 +1,13 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const N = +input[0];
+const sortedArr = input[1].split(" ").map(el => +el).sort((a,b) => a-b);
+let result = 0;
+// [1,2,3,3,4] 반복문으로 합을 구해보자.
+
+for(let i = 0; i < N; i++){
+   let findIndex = sortedArr.slice(0,i+1)
+   result += findIndex.reduce((acc,cur) => acc + cur)
+}
+
+console.log(result);
