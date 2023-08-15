eecd1c7a8f08222e8aaccbac8054b165caa1048b
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-08-16 00:49:05 +0900

    [Silver IV] Title: ATM, Time: 196 ms, Memory: 11800 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -6,7 +6,7 @@ let result = 0;
 // [1,2,3,3,4] 반복문으로 합을 구해보자.
 
 for(let i = 0; i < N; i++){
-   let findIndex = sortedArr.slice(0,i+1)
+   const findIndex = sortedArr.slice(0,i+1)
    result += findIndex.reduce((acc,cur) => acc + cur)
 }
 
