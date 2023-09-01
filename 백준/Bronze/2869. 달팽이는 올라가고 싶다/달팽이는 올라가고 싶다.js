065826ebd184359d37d67651cb2a1015ded60b38
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-09-01 22:35:26 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -1,6 +1,6 @@
 const [A,B,V] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
 
-let day = A - B;
+const day = A - B;
 const distance = V - B;
 
-day === 1 ? console.log(distance) : console.log(Math.ceil(distance / day));
+console.log(Math.ceil(distance / day));
