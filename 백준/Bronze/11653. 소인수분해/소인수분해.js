92699584be184e5cb85414e4971d7529b5201639
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-09-05 00:47:23 +0900

    [Bronze I] Title: 소인수분해, Time: 232 ms, Memory: 9932 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -2,7 +2,21 @@ let [N] = require("fs").readFileSync("/dev/stdin").toString().trim().split().map
 let quotient = 2;
 let result = [];
 
-while(N !== 1){
+    // 소수 판별
+    let index = 1;
+    while(index <= Math.sqrt(N)){
+        if(N % index === 0){
+          result.push(index);
+          if(N / index !== index) result.push(N / index);
+        } 
+        index++
+    }
+
+    if(result.length === 2){
+        console.log(N);
+    } else {
+        result = [];
+        while(N !== 1){
     if(N % quotient === 0){
         result.push(quotient);
         N /= quotient;
@@ -10,5 +24,5 @@ while(N !== 1){
         quotient++   
     }
 }
-
-result.forEach(el => console.log(el))
+    result.forEach(el => console.log(el))
+    }
