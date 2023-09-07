a0bda4c4acab0b78c21d9630231a2e31d514c52e
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-09-07 14:09:54 +0900

    [Bronze III] Title: 대지, Time: 248 ms, Memory: 22348 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.js
+++ 백준/Bronze/9063. 대지/대지.js
@@ -10,7 +10,4 @@ for(let i=1; i<=N; i++){
     y.push(arr[1]);
 }
 
-x.sort((a,b) => b - a);
-y.sort((a,b) => b - a);
-
-console.log((x[0] - x[x.length-1]) * (y[0] - y[y.length-1]));
+console.log((Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y)))
