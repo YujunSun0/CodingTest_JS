2707cd0d64de30b4247cebcf6d1aabc26e839128
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-09-12 20:07:23 +0900

    [Bronze II] Title: 블랙잭, Time: 172 ms, Memory: 9656 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -1,7 +1,10 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const stringToNumber = (str) => {
+    return str.split(" ").map(Number);
+}
 
-const [N,M] = input[0].split(" ").map(Number);
-let cards = input[1].split(" ").map(Number);
+const [N,M] = stringToNumber(input[0]);
+const cards = stringToNumber(input[1]);
 let max = 0;
 
 while(cards.length >= 3 && max !== M){
@@ -9,8 +12,8 @@ while(cards.length >= 3 && max !== M){
      for(let j=i+1; j<cards.length; j++){
       let sum = cards[0] + cards[i] + cards[j];
       if(sum > max && sum <= M) max = sum;
-    }   
-    }
+      }   
+     }
       cards.shift();
 }
 
