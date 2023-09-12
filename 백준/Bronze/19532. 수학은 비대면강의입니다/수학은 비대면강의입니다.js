974e694d4be37e826f0adc56808720db8d003f7f
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-09-12 22:54:28 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 116 ms, Memory: 9572 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.js
@@ -1,14 +1,6 @@
 const [a,b,c,d,e,f] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
 
-let [x,y] = [0,0];
-
-for(let i=-999; i<=999; i++){
-    for(let j=-999; j<=999; j++){
-        if((a*i)+(b*j) === c && (d*i)+(e*j) === f){
-         x += i;
-         y += j;   
-        }
-    }
-}
+const x = (b * f - e * c) / (b * d - e * a);
+const y = (a * f - d * c) / (a * e - d * b);
 
 console.log(`${x} ${y}`)
