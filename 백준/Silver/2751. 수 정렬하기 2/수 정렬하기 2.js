4c4b78b6da9dec93a93a4dcc9de0ee2cb5ab015e
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-09-16 23:58:48 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 912 ms, Memory: 201996 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -1,7 +1,22 @@
-console.log(require("fs")
+let input = require("fs")
 .readFileSync("/dev/stdin")
 .toString()
 .trim()
 .split("\n")
-.slice(1)
-.map(el => +el).sort((a,b) => a-b).join("\n"));
+.map(Number);
+
+let N = input.shift();
+let result = "";
+// 범위가 -1000000 ~ 1000000 이므로 수는 2000001가지가 나올 수 있다.
+let arr = new Array(2000001).fill(0);
+
+// index는 -가 될 수 없기 때문에 +1000000을 해준다.
+for(let i = 0; i < N; i++){
+    arr[input[i]+1000000]++;
+}
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] === 1) result += `${i-1000000}\n`;
+}
+
+console.log(result.trim());
