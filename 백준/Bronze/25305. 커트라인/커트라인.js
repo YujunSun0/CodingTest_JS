0ba9960ad616a7ebc6f58a30ba5a00d236abef2d
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-09-16 22:34:20 +0900

    [Bronze II] Title: 커트라인, Time: 120 ms, Memory: 9388 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N,k] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number).sort((a,b) => b-a);
+
+console.log(arr[k-1]);
+
