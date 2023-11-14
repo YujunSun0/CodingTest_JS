2e6c6ec321ee25cc54735c7c68cd98dd20d13ffe
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-11-14 23:57:47 +0900

    [Silver IV] Title: 등수 구하기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n, Eugene, p = map(int, input().split())
+
+    if n == 0:
+        print(1)
+    else:
+        ranking = list(map(int, input().split()))
+        if n == p and ranking[-1] >= Eugene:
+            print(-1)
+        else:
+            rank = n + 1
+            for i in range(n):
+                if ranking[i] <= Eugene:
+                    rank = i + 1
+                    break
+            print(rank)
