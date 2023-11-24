a795d84cd30cd0f393878006eacff96dca6d7643
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2023-11-24 13:03:47 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 21.34 ms, Memory: 48 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,23 @@
+function solution(queue1, queue2) {
+    const TOTAL_ARRAY = [...queue1, ...queue2];
+    const MAXCOUNT = 3*TOTAL_ARRAY.length;
+    const sum = (arr)=>arr.reduce((a,b)=>a+b);
+    const TARGET = sum(TOTAL_ARRAY)/2;
+    let count = 0;
+    let start = 0;
+    let end = queue1.length;
+    let totalSum = sum(TOTAL_ARRAY.slice(start, end));
+    while(count<=MAXCOUNT){
+        if(TARGET > totalSum){
+            totalSum += TOTAL_ARRAY[end];
+            end++;
+        }else if(TARGET < totalSum){
+            totalSum -= TOTAL_ARRAY[start];
+            start++;      
+        }else if(TARGET === totalSum){
+            return count;
+        }
+        count++;
+    }
+    return -1;
+}
