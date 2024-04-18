6fd1e94a0804e3f881a0bd443dcf932aa8829212
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2024-04-18 11:09:35 +0900

    [level 2] Title: 올바른 괄호, Time: 5.23 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,19 +1,17 @@
-function solution(s){
-    // "("와 ")"가 만나면 제거해준다. 순서를 중요시 해야 함
+function solution(s) {
     const stack = [];
-    const stringArray = s.split("");
-    let idx = 0;
+    const ArrayToS = s.split("");
+    let answer = true;
     
-    for(let str of stringArray){
-        stack.push(str);
-        // str이 ")"일 때, 이전의 str이 "("이면
-        if(str === ")" && stack[idx-1] === "(") {
-            // 해당하는 인덱스와 이전 인덱스의 요소 삭제
-            stack.splice(idx-1, 2);
-            idx -= 2;
+    // 여는 괄호만 스택에 저장 -> 닫는 괄호를 만나면 스택에서 하나씩 제거
+    ArrayToS.forEach(str => {
+        // pop : O(1), shift : O(n)
+        if(str === "("){
+            stack.push(str)
+        } else {
+            stack.length > 0 ? stack.pop() : answer = false;
         }
-        idx++
-    }
+    })
     
-    return stack.length === 0 ? true : false;
+    return answer && stack.length === 0;
 }
