67003d573ed5be44e17ae2ceec4d35fb8f9f5a2e
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2024-04-21 15:40:08 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -0,0 +1,43 @@
+function solution(n) {
+    const answer = Array.from({length:n}, () => []);
+    // 현재 행,열의 위치를 저장하는 변수
+    let [x,y,i] = [0,0,1];
+    let [low_left, low_right] = [0, n-1];
+    let [column_top, column_bottom] = [0, n-1];
+    let direction = "right";
+    while(i <= n**2) {
+        answer[y][x] = i;
+        switch(direction) {
+            case "left":
+                x--;
+                if(low_left === x){
+                  direction = "top";
+                  column_bottom--;
+                } 
+                break;
+            case "right":
+                x++;
+                if(low_right === x){
+                  direction = "bottom";
+                  column_top++;
+                } 
+                break;
+            case "top":
+                y--;
+                if(column_top === y){
+                  direction = "right";
+                  low_left++;
+                } 
+                break;
+            case "bottom":
+                y++;
+                if(column_bottom === y){
+                  direction = "left";
+                  low_right--;
+                } 
+                break;
+        }
+        i++;
+    }
+    return answer;
+}
