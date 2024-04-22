6e54c4ae5dc68e7ad2263c05fe3e38569e7c186f
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2024-04-22 15:20:27 +0900

    [level 2] Title: H-Index, Time: 2.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -2,6 +2,7 @@ function solution(citations) {
   citations.sort((a, b) => b - a);
   
   for (let i = 0; i < citations.length; i++) {
+      console.log(citations[i], i + 1);
     if (citations[i] === i + 1) {
       return i + 1;
     } else if (citations[i] < i + 1) {
