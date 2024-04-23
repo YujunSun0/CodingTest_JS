a9d2c14f7fbf088d6fda273f480ed1812751a118
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2024-04-23 14:00:20 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,43 +1,49 @@
 function solution(begin, target, words) {
-    // dfs(word, cnt, words);
-    // words를 순회하며 word.length-1의 수 만큼 같은 스펠링이라면
-    // words에서 빼고 dfs(el, cnt + 1, words) 실행
-    // target이랑 word가 같을 때 answer 배열에 cnt push
-    // cnt 배열에서 최솟값 리턴
     words.sort();
     const dfs = (word, cnt, words) => {
-        // base case
-        // 변환이 완료되었으면 횟수를 리턴
-        console.log(cnt, word, target);
-        if(word === target) {
+        // base case 1: 변환이 완료되었으면 변환 횟수를 반환
+        if (word === target) {
             return cnt;
         }
-        // 변환할 수 없는 경우 0 리턴 (target이 없을 때)
-        if(words.indexOf(target) === -1) {
+
+        // base case 2: 목표 단어가 단어 목록에 없는 경우 0을 반환
+        if (words.indexOf(target) === -1) {
             return 0;
+        }
+        
+        // base case 3: word에서 target으로 변경이 가능할 때 cnt + 1 반환
+        if(isConnected(word,target)) {
+            return cnt + 1;
         }
         
         let nextWord = "";
         // 변환 가능한 단어 찾기
-        for(let i=0; i<words.length; i++) {
-            const prev = word.split("");
-            const next = words[i].split("");
-            // 같은 위치의 같은 스펠링이 아닌 것만 반환 => 길이가 1이면 변경 가능
-            if(prev.filter((el,idx) => el !== next[idx]).length === 1) {
+        for (let i = 0; i < words.length; i++) {
+            // 두 문자열이 교환 가능하다면
+            if(isConnected(word,words[i])) {
                 const idx = words.indexOf(words[i]);
-                const spliceWord = words.splice(idx,1);
-                nextWord += spliceWord;
+                const spliceWord = words.splice(idx, 1);
+                nextWord = spliceWord[0];
                 break;
             }
         }
-        // 만약 반복문을 돌아서 nextWord가 빈 문자열이면 0 반환
-        if(nextWord.length === 0) return 0;
-    
-        // 아니면 dfs 실행
-        console.log(nextWord, cnt + 1, words);
-        const result = dfs(nextWord, cnt + 1, words);
-        return result;
-    }
-    const answer = dfs(begin, 0, words);
-    return answer;
+
+        // 변환 가능한 단어를 찾지 못한 경우 0을 반환
+        if (nextWord.length === 0) {
+            return 0;
+        }
+
+        // 재귀 호출하여 다음 단어로 변환
+        return dfs(nextWord, cnt + 1, words);
+    };
+
+    return dfs(begin, 0, words);
+}
+
+// 두 문자열 중 다른 스펠링이 하나일 경우 true
+const isConnected = (str1, str2) => {
+      const prev = str1.split("");
+      const next = str2.split("");
+        // 같은 위치의 같은 스펠링이 아닌 것만 반환 => 길이가 1이면 변경 가능
+      return prev.filter((el, idx) => el !== next[idx]).length === 1 ? true : false;
 }
