365385e36e4d6fcde25ec1bdad7e8c26aaa26478
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2024-06-25 23:09:05 +0900

    [level 2] Title: 짝지어 제거하기, Time: 23.84 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const stack = [];
+    
+    for (let i = 0; i < s.length; i++) { 
+        if(stack[stack.length - 1] === s[i]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    
+    return stack.length ? 0 : 1;
+}
