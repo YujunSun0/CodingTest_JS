f8a28f14467d51d3a1bb78ebacb03a205f9c8176
YujunSun0 <120611048+YujunSun0@users.noreply.github.com>
2024-11-01 22:07:27 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -2,9 +2,7 @@ a, b, c = map(int, input().split())
 
 if a==b==c:
     print(10000+a*1000)
-elif a==b:
-    print(1000+a*100)
-elif a==c:
+elif a==b or a==c:
     print(1000+a*100)
 elif b==c:
     print(1000+b*100)
